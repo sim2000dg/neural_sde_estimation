7111fb2012a4a3af50f62b0c097bc10ebc14b1b9
sim2000dg <sim2000dg@gmail.com>
2024-10-13 22:18:18 +0200

    Minor changes in import modules

--- neural_sde/__init__.py
+++ neural_sde/__init__.py
@@ -1,2 +1,2 @@
-from .neural_fit import grid_test
+from .neural_fit import grid_test, _model_fit
 from .simulation_sde import milstein_sim, euler_sim, SinusoidDriftSigmoidDiffusion, SDECoefficient, test_compact
--- neural_sde/neural_fit/__init__.py
+++ neural_sde/neural_fit/__init__.py
@@ -1,1 +1,2 @@
 from .model_fit import grid_test
+from .model_fit import model_fit_routine as _model_fit
